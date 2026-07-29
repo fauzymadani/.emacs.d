42b156de8c4d3940abe6735aa309d4f20b8bbb8f
fauzymadani <keperluansekolahfauzy@gmail.com>
2026-07-29 19:28:15 +0700

    Stop tracking session state (history, places, fontaine leftover)

--- custom.el
+++ custom.el
@@ -5,7 +5,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(apropospriate-theme async auctex avy casual cdlatex corfu dashboard
+                         dired-preview ef-themes embark-consult
+                         fontaine gruber-darker-theme ledger-mode
+                         logos marginalia mixed-pitch modusregel
+                         mood-line move-text multiple-cursors olivetti
+                         orderless org-appear org-journal org-modern
+                         org-tree-slide popper pulsar solarized-theme
+                         tmr uwu-theme vertico vundo wgrep yasnippet))
  '(package-vc-selected-packages
    '((org-mode :url "https://code.tecosaur.net/tec/org-mode" :branch
                "dev"))))
