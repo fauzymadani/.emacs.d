7585a905dd0daa17c31c69a87f5b47b0d163a287
fauzymadani <keperluansekolahfauzy@gmail.com>
2026-08-03 19:41:51 +0700

    Drop duplicate Customize blocks from init.el

--- init.el
+++ init.el
@@ -1032,24 +1032,3 @@ White text; the label \"end\" is gold and \"re\" is green."
 ;; Keep Customize's auto-writes out of init.el
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(apropospriate-theme async auctex avy casual cdlatex corfu dashboard
-			 dired-preview doom-themes ef-themes elfeed
-			 embark-consult f fontaine gruber-darker-theme
-			 keycast ledger-mode logos marginalia
-			 mixed-pitch modusregel mood-line move-text
-			 multiple-cursors olivetti orderless
-			 org-appear org-journal org-modern
-			 org-tree-slide popper pulsar solarized-theme
-			 tmr uwu-theme vertico vundo wgrep yasnippet)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
