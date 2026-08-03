3826d4920dbea24903bf95294b37b9cda8b3ade1
fauzymadani <keperluansekolahfauzy@gmail.com>
2026-08-03 19:36:43 +0700

    Drop dream-theme

--- custom.el
+++ custom.el
@@ -5,7 +5,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(apropospriate-theme async auctex avy casual cdlatex corfu dashboard
+                         dired-preview doom-themes
+                         ef-themes elfeed embark-consult f fontaine
+                         gruber-darker-theme keycast ledger-mode logos
+                         marginalia mixed-pitch modusregel mood-line
+                         move-text multiple-cursors olivetti orderless
+                         org-appear org-journal org-modern
+                         org-tree-slide popper pulsar solarized-theme
+                         tmr uwu-theme vertico vundo wgrep yasnippet))
  '(package-vc-selected-packages
    '((org-mode :url "https://code.tecosaur.net/tec/org-mode" :branch
                "dev")))
